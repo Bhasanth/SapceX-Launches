78adfa32c8feb03009e6baf971974c04afca432a
Bhasanth <slakkaraju@ufll.edu>
2020-04-23 17:11:34 -0400

    Finished App with Apollo and ReactJS UI

---
+++ public/precache-manifest.c7afd89fcece551b69cecd895c3bf357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325e6cbe559665047ca4774c3be93cb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa3b6abeb757bdbb4de",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "45dd82ab447e25d0a45f",
+    "url": "/static/js/2.8bdfc6ac.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.8bdfc6ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa3b6abeb757bdbb4de",
+    "url": "/static/js/main.032a6e7b.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "6b7cbac7b67058e2ee065f596909093c",
+    "url": "/static/media/logo.6b7cbac7.jpg"
+  }
+]);
